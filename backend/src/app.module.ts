95440fac1bbe8477001520c4c711c03dc74e8aef
AbIgneIgnem <pppp43777@gmail.com>
2022-03-10 21:59:13 +0300

    add track module, service, controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { ConfigModule } from '@nestjs/config';
 import { FileModule } from './file/file.module';
-
 @Module({
-  imports: [FileModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    MongooseModule.forRoot(process.env.MONGODB),
+    FileModule,
+    TrackModule,
+  ],
+  controllers: [],
 })
 export class AppModule {}
